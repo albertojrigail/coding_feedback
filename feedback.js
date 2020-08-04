3b5e144552b8a68e1475d7ff3dff14b229c7589e
Alberto Rigail <albertojrigail@hotmail.com>
2020-08-04 01:59:43 -0500

    integrate code snippets into facebook messenger

--- feedback.js
+++ feedback.js
@@ -1,65 +1,98 @@
 'use strict';
+const request = require('request');
+const axios = require('axios').default;
+
+const TWENTY_SECONDS = 5000;
 
 const start = (say, sendButton) => {
-    // const rand= Math.floor(Math.random() * 10);     // returns a random integer from 0 to 9
-	const rand = 0;
-	const str = '*CODING PUZZLE:* \n\n' + puzzlesText[rand] + '\n\n Take your time to read the prompt thoroughly';
-	say(str).then(() => {
-		await new Promise(resolve => setTimeout(resolve, 2000));
-		state('no', 'Are you ready to start coding?', ['yes', 'no']);
-	});	
-	
+	say('Take your time to read the prompt thoroughly...').then(() => {
+		const now = Date.now();
+
+		// GET RANDOM PROBLEM
+		// make request to get problem from database
+		request('http://34.96.245.124:2999/problem', function(err, res, body) {
+			let json = JSON.parse(body);
+			let problemId = json['_id'];
+			let userId = 'albertojrigail';
+			let title = json['title'];
+			let prompt = json['prompt'];
+			let solutionTemplate = json['solution_template'];
+			
+			let url = 'http://34.96.245.124:2999/?uid=' + userId + '&pid=' + problemId +'&datetime=' + now;
+
+			let script = '<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?lang=py&amp;skin=sunburst"></script>';
+			let starthtml = '<pre class="prettyprint">';
+			let middlehtml = '';
+			solutionTemplate.forEach(element => {
+				middlehtml += element + '<br/>';
+			});
+			let endhtml = '</pre>';
+			
+			say('*'+title+'*').then(() => {
+				say(prompt).then(() => {
+					// GET IMAGE
+					// Make request to get image snippet
+					const data = {
+						html: script + starthtml + middlehtml + endhtml,
+						css: "",
+						google_fonts: "Roboto"
+					}
+					
+					const API_ID = "ca2e9be6-9728-4f7b-a46c-1cdce4bc0676";
+					const API_KEY= "f32f2ad1-7ac7-48bc-8e51-4bab98348502";
+					
+					// Create an image by sending a POST to the API.
+					// Retrieve your api_id and api_key from the Dashboard. https://htmlcsstoimage.com/dashboard
+					request.post(
+						{
+							url:'https://hcti.io/v1/image',
+							form: data,
+							auth: {
+								user: API_ID,
+								pass: API_KEY,
+							}
+						}, function(error,response,newBody) {
+							let imageUrl = JSON.parse(newBody)["url"];
+							say("This is a template for the solution:").then(() => {
+								say({
+									attachment: "image",
+									url: imageUrl,
+								});
+							})
+						}
+					);
+				});
+			})
+		});
+
+		
+	})
 };
 
 
-const state = (payload, say, sendButton) => {
+const state = (payload, say) => {
 	if(payload === 'yes') {
-		say('Great! You will be redirected a website for your submission!').then(() => {
-			window.open()
-
+		say('Great! You should open this website for submission').then(() => {
 		});
 	} else {
-		say('No problem, take your time!').then(() => {
-			await new Promise(resolve => setTimeout(resolve, 4000));
-			state('no', 'Are you ready now?', ['yes', 'no']);
+		say('That is fine. Take your time...').then(() => {
+			setTimeout(() => {}, TWENTY_SECONDS).then(() => {
+				sendButton('Are you ready to start coding?', ['yes', 'no'])
+			});
 		});
 	}
-
-
-	say({attachment:'image', url: solutionPhoto[0],}).then(() => {
-		say({text:'🎉 Congratulations! You have finished the challenge!'}).then(() => {
-			sendButton('Play again?', [
-				{title: 'Yes!', payload: 'restart'},
-				'No'
-			]);
-		});
-	});
-	
 };
 
 module.exports = {
-	filename: 'bubblesort',
+	filename: 'challenge1',
 	title: 'Coding Puzzle Feedback',
 	introduction: [
-		"Hi! Let's do a coding puzzle!/n ",
-		'I will show you a coding puzzle question, and you should provide the code for the solution.\n',
-		'After your submission, you may be asked to give feedback to a classmate.\n',
-		'If you do, you may receive feedback on your solution.\n',
+		"Hi! Let's do a coding puzzle!\n\n" +
+		'I will show you a coding puzzle question, and you should provide the code for the solution. ' +
+		'After your submission, you may be asked to give feedback to a classmate.' +
+		'If you do, you may receive feedback on your solution.\n\n' +
 		'Good Luck!'		
     	],
 	start: start,
-	state: state
+	state: state,
 };
-
-// Section for Data
-
-// This list contains the prompts for several coding puzzles
-const puzzles = [
-    "You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. \n Return the destination city, that is, the city without any path outgoing to another city.\n" +
-    "It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.",
-]
-
-// contains images for sample solutions of coding puzzles
-var solutionPhoto = [
-    'https://4.bp.blogspot.com/-w5mxkIEGAt8/XI1DXbCCYBI/AAAAAAAAAPA/nyc478tI0Fg43ikj_YeWllrCyOSCtWmGACLcBGAs/w800/fin.png',
-]
